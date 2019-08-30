9eac0373777d9f86f79fee58e5b927e23010ea20
afozbek <furkanozbek1995@gmail.com>
2019-08-30 18:59:16 +0300

    adding header component

---
+++ src/components/Header.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="row">
+        <div class="col-sm-12">
+            <h3>Quotes Added</h3>
+            <div class="progress">
+                <div
+                    class="progress-bar"
+                    role="progressbar"
+                    aria-valuenow="60"
+                    aria-valuemin="0"
+                    aria-valuemax="100"
+                    :style="calculateProgressWidth"
+                >
+                    {{ quoteCount }} / {{ maxQuotes }}
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ["quoteCount", "maxQuotes"],
+    computed: {
+        calculateProgressWidth() {
+            let widthPercent = (this.quoteCount / this.maxQuotes) * 100
+
+            return { width: widthPercent + "%" }
+        }
+    }
+}
+</script>
+
+<style></style>
