538b4d89a69b8a381689feb7f09f931b0e3c6aeb
afozbek <furkanozbek1995@gmail.com>
2019-08-30 18:37:34 +0300

    adding creation & deletion functionality

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,40 @@
 <template>
     <div class="container">
-        <quote-grid :quotes="quotes" />
+        <app-new-quote @quoteCreated="createQuote"></app-new-quote>
+        <app-quote-grid :quotes="quotes" @deleteQuote="deleteQuote" />
+
+        <!-- INFO BOX -->
+        <div class="col-sm-12 text-center">
+            <div class="alert alert-info">
+                INFO: Click on a Quote to delete it!
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import QuoteGrid from "./components/QuoteGrid.vue"
+import NewQuote from "./components/NewQuote.vue"
+import { uuid } from "vue-uuid"
 
 export default {
     data() {
         return {
             maxQuote: 10,
-            quotes: [{ id: 1, text: "Simple quote" }]
+            quotes: [{ id: uuid.v1(), quote: "Simple quote" }]
         }
     },
     components: {
-        quoteGrid: QuoteGrid
+        appQuoteGrid: QuoteGrid,
+        appNewQuote: NewQuote
+    },
+    methods: {
+        createQuote(quote) {
+            this.quotes = [...this.quotes, { id: uuid.v1(), quote }]
+        },
+        deleteQuote(quoteId) {
+            this.quotes = this.quotes.filter(quote => quote.id !== quoteId)
+        }
     }
 }
 </script>
